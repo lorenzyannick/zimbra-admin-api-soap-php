fbef549662300851f6d36734856880a14b98aec9
yaminator <yaminator@4db38442-eaef-11de-9229-05bbbb8527c3>
2009-12-17 12:52:12 +0000

    2nd commit, test

--- trunk/zm/config.php
+++ trunk/zm/config.php
@@ -1,11 +1,12 @@
 <?php
 
-$config_server = "zmd.1g6.biz";
+$zimbraserver = "zmd.1g6.biz";
 $zimbraadminemail = "admin@zmd.1g6.biz";
 $zimbraadminpassword = "IgbDh2GN";
 
-$config_uri = "urn:zimbraAdmin";
-$config_urn = "urn:zimbra";
+// Not yet implemented
+//$config_uri = "urn:zimbraAdmin";
+//$config_urn = "urn:zimbra";
 
 
 ?>
